ee371c266c5a20fb09b4f3a33e4b86d57ce78af3
MahidharBandaru <mahi.mahidhar2@gmail.com>
2020-02-16 14:14:55 +0530

    initial commit

---
+++ server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+
+int main () {
+    
+    return 0;
+}
