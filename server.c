50901bfce00d0c0b370d1170681c139d3135fb50
MahidharBandaru <mahi.mahidhar2@gmail.com>
2020-02-16 21:15:14 +0530

    Single group chat

--- server.c
+++ server.c
@@ -14,26 +14,294 @@
 
 #define MAX_BUFFER 1024
 #define MAX_CHATBOXES 10
+
 typedef struct {
     char *buffer[MAX_BUFFER];
     int head, tail;
     int full, empty;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *notFull, *notEmpty;
 } queue;
 
 typedef struct {
+    fd_set serverReadFds;
     int socketFd;
     int clientSockets[MAX_BUFFER];
-    char* clientNames[MAX_BUFFER];
     int numClients;
+    pthread_mutex_t *clientListMutex;
     queue *queue;
 } chatDataVars;
 
 typedef struct {
+    chatDataVars *data;
+    int clientSocketFd;
+} clientHandlerVars;
+
+
+typedef struct {
     chatDataVars chatBox[MAX_CHATBOXES];
-    char *chatBoxName[MAX_CHATBOXES];
+    char* chatBoxName[MAX_CHATBOXES];
+    int tally;
+    pthread_mutex_t *chatBoxListMutex;
 } chatData;
 
-int main () {
+void buildMessage(char *result, char *name, char *msg);
+void removeClient(chatDataVars *data, int clientSocketFd);
+
+void *newClientHandler(void *data);
+void *clientHandler(void *chv);
+void *messageHandler(void *data);
+
+//Initializes queue
+queue* queueInit(void)
+{
+    queue *q = (queue *)malloc(sizeof(queue));
+
+    q->empty = 1;
+    q->full = q->head = q->tail = 0;
+    q->mutex = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(q->mutex, NULL);
+
+    q->notFull = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(q->notFull, NULL);
+
+    q->notEmpty = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(q->notEmpty, NULL);
+
+    return q;
+}
+
+
+//Frees a queue
+void queueDestroy(queue *q)
+{
+    pthread_mutex_destroy(q->mutex);
+    pthread_cond_destroy(q->notFull);
+    pthread_cond_destroy(q->notEmpty);
+    free(q->mutex);
+    free(q->notFull);
+    free(q->notEmpty);
+    free(q);
+}
+
+//Push to end of queue
+void queuePush(queue *q, char* msg)
+{
+    q->buffer[q->tail] = msg;
+    q->tail++;
+    if(q->tail == MAX_BUFFER)
+        q->tail = 0;
+    if(q->tail == q->head)
+        q->full = 1;
+    q->empty = 0;
+}
+
+//Pop front of queue
+char* queuePop(queue *q)
+{
+    char* msg = q->buffer[q->head];
+    q->head++;
+    if(q->head == MAX_BUFFER)
+        q->head = 0;
+    if(q->head == q->tail)
+        q->empty = 1;
+    q->full = 0;
+
+    return msg;
+}
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in serverAddr;
+    long port = 9999;
+    int socketFd;
+
+    if(argc == 2) port = strtol(argv[1], NULL, 0);
+
+    if((socketFd = socket(AF_INET, SOCK_STREAM, 0))== -1)
+    {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    // binding socket
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(port);
+
+    if(bind(socketFd, (struct sockaddr *)(&serverAddr), sizeof(struct sockaddr_in)) == -1)
+    {
+        perror("Socket bind failed: ");
+        exit(1);
+    }
+
+    // listening on the socket
+    if(listen(socketFd, 1) == -1)
+    {
+        perror("listen failed: ");
+        exit(1);
+    }
+
+    chatDataVars data;
+    data.numClients = 0;
+    data.socketFd = socketFd;
+    data.queue = queueInit();
+    data.clientListMutex = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(data.clientListMutex, NULL);
+
+    //Start thread to handle new client connections
+    pthread_t connectionThread;
+    if((pthread_create(&connectionThread, NULL, (void *)&newClientHandler, (void *)&data)) == 0)
+    {
+        fprintf(stderr, "Connection handler started\n");
+    }
+
+    FD_ZERO(&(data.serverReadFds));
+    FD_SET(socketFd, &(data.serverReadFds));
+
+    //Start thread to handle messages received
+    pthread_t messagesThread;
+    if((pthread_create(&messagesThread, NULL, (void *)&messageHandler, (void *)&data)) == 0)
+    {
+        fprintf(stderr, "Message handler started\n");
+    }
+
+    pthread_join(connectionThread, NULL);
+    pthread_join(messagesThread, NULL);
+
+    queueDestroy(data.queue);
+    pthread_mutex_destroy(data.clientListMutex);
+    free(data.clientListMutex);
     
-    return 0;
+    close(socketFd);
+}
+
+//Removes the socket from the list of active client sockets and closes it
+void removeClient(chatDataVars *data, int clientSocketFd)
+{
+    pthread_mutex_lock(data->clientListMutex);
+    for(int i = 0; i < MAX_BUFFER; i++)
+    {
+        if(data->clientSockets[i] == clientSocketFd)
+        {
+            data->clientSockets[i] = 0;
+            close(clientSocketFd);
+            data->numClients--;
+            break;
+        }
+    }
+    pthread_mutex_unlock(data->clientListMutex);
+}
+
+//Thread to handle new connections. Adds client's fd to list of client fds and spawns a new clientHandler thread for it
+void *newClientHandler(void *data)
+{
+    chatDataVars *chatData = (chatDataVars *) data;
+    while(1)
+    {
+        int clientSocketFd = accept(chatData->socketFd, NULL, NULL);
+        if(clientSocketFd > 0)
+        {
+            fprintf(stderr, "Server accepted new client. Socket: %d\n", clientSocketFd);
+
+            //Obtain lock on clients list and add new client in
+            pthread_mutex_lock(chatData->clientListMutex);
+            if(chatData->numClients < MAX_BUFFER)
+            {
+                //Add new client to list
+                for(int i = 0; i < MAX_BUFFER; i++)
+                {
+                    if(!FD_ISSET(chatData->clientSockets[i], &(chatData->serverReadFds)))
+                    {
+                        chatData->clientSockets[i] = clientSocketFd;
+                        i = MAX_BUFFER;
+                    }
+                }
+
+                FD_SET(clientSocketFd, &(chatData->serverReadFds));
+
+                //Spawn new thread to handle client's messages
+                clientHandlerVars chv;
+                chv.clientSocketFd = clientSocketFd;
+                chv.data = chatData;
+
+                pthread_t clientThread;
+                if((pthread_create(&clientThread, NULL, (void *)&clientHandler, (void *)&chv)) == 0)
+                {
+                    chatData->numClients++;
+                    fprintf(stderr, "Client has joined chat. Socket: %d\n", clientSocketFd);
+                }
+                else
+                    close(clientSocketFd);
+            }
+            pthread_mutex_unlock(chatData->clientListMutex);
+        }
+    }
+}
+
+void *clientHandler(void *chv)
+{
+    clientHandlerVars *vars = (clientHandlerVars *)chv;
+    chatDataVars *data = (chatDataVars *)vars->data;
+
+    queue *q = data->queue;
+    int clientSocketFd = vars->clientSocketFd;
+
+    char msgBuffer[MAX_BUFFER];
+    while(1)
+    {
+        int numBytesRead = read(clientSocketFd, msgBuffer, MAX_BUFFER - 1);
+        msgBuffer[numBytesRead] = '\0';
+
+        //If the client sent /exit\n, remove them from the client list and close their socket
+        if(strcmp(msgBuffer, "/exit\n") == 0)
+        {
+            fprintf(stderr, "Client on socket %d has disconnected.\n", clientSocketFd);
+            removeClient(data, clientSocketFd);
+            return NULL;
+        }
+        else
+        {
+            while(q->full)
+            {
+                pthread_cond_wait(q->notFull, q->mutex);
+            }
+
+            pthread_mutex_lock(q->mutex);
+            fprintf(stderr, "Pushing message to queue: %s\n", msgBuffer);
+            queuePush(q, msgBuffer);
+            pthread_mutex_unlock(q->mutex);
+            pthread_cond_signal(q->notEmpty);
+        }
+    }
+}
+
+void *messageHandler(void *data)
+{
+    chatDataVars *chatData = (chatDataVars *)data;
+    queue *q = chatData->queue;
+    int *clientSockets = chatData->clientSockets;
+
+    while(1)
+    {
+        //Obtain lock and pop message from queue when not empty
+        pthread_mutex_lock(q->mutex);
+        while(q->empty)
+        {
+            pthread_cond_wait(q->notEmpty, q->mutex);
+        }
+        char* msg = queuePop(q);
+        pthread_mutex_unlock(q->mutex);
+        pthread_cond_signal(q->notFull);
+
+        //Broadcast message to all connected clients
+        fprintf(stderr, "Broadcasting message: %s\n", msg);
+        for(int i = 0; i < chatData->numClients; i++)
+        {
+            int socket = clientSockets[i];
+            if(socket != 0 && write(socket, msg, MAX_BUFFER - 1) == -1)
+                perror("Socket write failed: ");
+        }
+    }
 }
